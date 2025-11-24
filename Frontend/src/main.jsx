dd88eb795c006a9650545c28f504305b5946bbe9
Tony555x <antonkremov2007@gmail.com>
2025-11-24 20:06:03 +0200

    react routing

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,12 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Dashboard from './pages/Dashboard.jsx'
-import Navbar from './pages/Navbar.jsx'
+import App from './App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Navbar />
-    <Dashboard />
+    <App />
   </StrictMode>,
 )
